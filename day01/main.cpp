0b06650a3081d8e365347ac9705df54cfdb750cd
Adam Koleszar <adk@excitor.com>
2016-12-02 12:16:35 +0100

    Day 1b

--- day01/main.cpp
+++ day01/main.cpp
@@ -12,77 +12,111 @@ struct Step {
     x0 = a0; y0 = b0; x1 = a1; y1 = b1;
   }
   bool crossCheck(const Step& other, Step& cross) {
-    if (x1 > x0 && x0 < other.x0 && other.x0 <= x1) { //EAST
-      // if (other.x0 == other.x1) {
-      //   if (x0 < other.x0 && other.x0 <= x1) {
-      //     cross.set(other.x0, y0, other.x0, y0);
-      //     return true;
-      //   }
-      // } else if (other.x0 < x1 && x1 <= other.x1) {
-      //   cross.set(other.x0, y0, other.x0, y0);
-      //   return true;
-      // }
-      cross.set(other.x0, y0, other.x0, y0);
+    std::cout << "(" << x0 << "," << y0 << "->" << x1 << "," << y1 << ") X (";
+    std::cout << other.x0 << "," << other.y0 << "->" << other.x1 << "," << other.y1 << ")" << std::endl;
+    //EAST
+    if ((x1 > x0) &&
+        (x0 < other.x0 && other.x0 < x1 || x0 < other.x1 && other.x1 < x1) &&
+        (other.y0 <= y0 && y0 <= other.y1 || other.y1 <= y0 && y0 <= other.y0)) {
+      int cx0 = (other.x0 <= other.x1) ? other.x0 : other.x1;
+      cross.set(cx0, y0, cx0, y0);
       return true;
-    } else if (x1 < x0 && x1 <= other.x0 && other.x0 < x0)
+    }
+    //WEST
+    if ((x0 > x1) &&
+        (x1 < other.x0 && other.x0 < x0 || x1 < other.x1 && other.x1 < x0) &&
+        (other.y0 <= y0 && y0 <= other.y1 || other.y1 <= y0 && y0 <= other.y0)) {
+      int cx0 = (other.x0 >= other.x1) ? other.x0 : other.x1;
+      cross.set(cx0, y0, cx0, y0);
+      return true;
+    }
+    //NORTH
+    if ((y0 < y1) &&
+        (y0 < other.y0 && other.y0 < y1 || y0 < other.y1 && other.y1 < y1) &&
+        (other.x0 <= x0 && x0 <= other.x1 || other.x1 <= x0 && x0 <= other.x0)) {
+      int cy0 = (other.y0 <= other.y1) ? other.y0 : other.y1;
+      cross.set(x0, cy0, x0, cy0);
+      return true;
+    }
+    //SOUTH
+    if ((y0 > y1) &&
+        (y1 < other.y0 && other.y0 < y0 || y1 < other.y1 && other.y1 < y0) &&
+        (other.x0 <= x0 && x0 <= other.x1 || other.x1 <= x0 && x0 <= other.x0)) {
+      int cy0 = (other.y0 >= other.y1) ? other.y0 : other.y1;
+      cross.set(x0, cy0, x0, cy0);
+      return true;
+    }
     return false;
   }
 };
 
 class Walker {
 public:
-  Walker() : ft(), steps() {
+  Walker() : ft(), steps(), firstTwice(false) {
     Step step;
-    steps.push_back(step);
   }
   void instruction(const std::string& inst) {
-    std::string steps = inst.substr(1);
-    if (inst.c_str()[0] == 'L') {
-      goLeft(std::stoi(steps));
-    } else {
-      goRight(std::stoi(steps));
+    Step s;
+    if (!steps.empty()) {
+      s = steps.back();
     }
+    Step next;
+    std::string cmd = inst.substr(1);
+    if (inst.c_str()[0] == 'L') {
+      next = goLeft(s, std::stoi(cmd));
+    } else {
+      next = goRight(s, std::stoi(cmd));
+    }
+    if (!firstTwice) {
+      crossCheck(next);
+    }
+    steps.push_back(next);
   }
   int getShortestPath() {
     Step s = steps.back();
     return abs(s.x1) + abs(s.y1);
   }
   int getFirstTwice() {
-    return abs(ft.x1) + abs(ft.y1);
+    std::cout << "(" << ft.x1 << "," << ft.y1 << ")" << std::endl;
+    return (firstTwice) ? abs(ft.x1) + abs(ft.y1) : -1;
   }
 
 private:
-  void goLeft(int step) {
-    Step s = steps.back();
+  Step goLeft(const Step& s, int step) {
     Step next;
     if (s.x1 > s.x0) { //EAST
       next.set(s.x1, s.y1, s.x1, s.y1+step);
     } else if (s.x1 < s.x0) { //WEST
       next.set(s.x1, s.y1, s.x1, s.y1-step);
-    } else if (s.y1 > s.y0) { //SOUTH
-      next.set(s.x1, s.y1, s.x1-step, s.y1);
-    } else { //NORTH
+    } else if (s.y1 < s.y0) { //SOUTH
       next.set(s.x1, s.y1, s.x1+step, s.y1);
+    } else { //NORTH
+      next.set(s.x1, s.y1, s.x1-step, s.y1);
     }
-    steps.push_back(next);
+    return next;
   }
-  void goRight(int step) {
-    Step s = steps.back();
+  Step goRight(const Step& s, int step) {
     Step next;
     if (s.x1 > s.x0) { //EAST
       next.set(s.x1, s.y1, s.x1, s.y1-step);
     } else if (s.x1 < s.x0) { //WEST
       next.set(s.x1, s.y1, s.x1, s.y1+step);
-    } else if (s.y1 > s.y0) { //SOUTH
-      next.set(s.x1, s.y1, s.x1+step, s.y1);
-    } else { //NORTH
+    } else if (s.y1 < s.y0) { //SOUTH
       next.set(s.x1, s.y1, s.x1-step, s.y1);
+    } else { //NORTH
+      next.set(s.x1, s.y1, s.x1+step, s.y1);
     }
-    steps.push_back(next);
+    return next;
+  }
+  void crossCheck(Step& s) {
+    for (std::list<Step>::iterator i = steps.begin(); i != steps.end() && !firstTwice; ++i) {
+      firstTwice |= s.crossCheck(*i, ft);
+    }
   }
 
   Step ft;
   std::list<Step> steps;
+  bool firstTwice;
 };
 
 void tokenize(const std::string& data, const std::string& delim, std::list<std::string>& l) {
@@ -113,5 +147,6 @@ int main(int argc, char* argv[]) {
     instructions.pop_front();
   }
 
-  std::cout << "Result: " << walker.getShortestPath() << std::endl;
+  std::cout << "Shortest path: " << walker.getShortestPath() << std::endl;
+  std::cout << "Shortest path to first cross: " << walker.getFirstTwice() << std::endl;
 }
