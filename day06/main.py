92b0e3db9f9cbcf929bd2285c20126000e0590c8
Adam Koleszar <adam.koleszar@sclable.com>
2018-12-08 10:13:07 +0100

    Day 06, 08:wip

--- day06/main.py
+++ day06/main.py
@@ -1,10 +1,86 @@
 import sys
+import re
 
-def part1(data):
-    lines = data.split('\n')
+def bounds(x_coords, y_coords):
+    min_x = x_coords[0]
+    max_x = x_coords[0]
+    min_y = y_coords[0]
+    max_y = y_coords[0]
+    for i in range(len(x_coords)):
+        min_x = min(min_x, x_coords[i])
+        min_y = min(min_y, y_coords[i])
+        max_x = max(max_x, x_coords[i])
+        max_y = max(max_y, y_coords[i])
+    return (min_x, min_y, max_x, max_y)
 
-def part2(data):
+def distance(x1, y1, x2, y2):
+    return abs(x1-x2) + abs(y1-y2)
+
+def min_distance_from_bounds(x, y, b):
+    return min(min(abs(b[0]-x), abs(b[2]-x)), min(abs(b[1]-y), abs(b[3]-y)))
+
+def part12(data):
     lines = data.split('\n')
+    x_coords = []
+    y_coords = []
+    for line in lines:
+        m = re.match(r'(\d+),\ (\d+)', line)
+        x_coords.append(int(m.group(1)))
+        y_coords.append(int(m.group(2)))
+
+    b = bounds(x_coords, y_coords)
+    size_x = b[2] - b[0]
+    size_y = b[3] - b[1]
+    for i in range(len(x_coords)):
+        x_coords[i] -= b[0]
+        y_coords[i] -= b[1]
+    grid = [-1] * size_x * size_y
+
+    middle_size = 0
+
+    # build the grid
+    for i in range(size_x):
+        for j in range(size_y):
+            min_dist = size_x + size_y
+            sum = 0
+            for l in range(len(x_coords)):
+                dist = distance(i, j, x_coords[l], y_coords[l])
+                sum += dist
+                if dist < min_dist:
+                    min_dist = dist
+                    grid[j*size_x + i] = l
+                elif min_dist == dist:
+                    grid[j*size_x + i] = -1
+            if sum < 10000:
+                middle_size += 1
+    # print(grid)
+
+
+    # cut the infinites
+    infinites = set()
+    for i in range(size_x):
+        infinites.add(grid[i])
+        infinites.add(grid[i+(size_y-1)*size_x])
+
+    for i in range(size_y):
+        infinites.add(grid[i*size_x])
+        infinites.add(grid[size_x-1 + i*size_x])
+    # print(infinites)
+
+    # count areas
+    areas = {}
+    for i in range(size_x*size_y):
+        if grid[i] not in infinites:
+            if grid[i] not in areas:
+                areas[grid[i]] = 0
+            areas[grid[i]] += 1
+    # print(areas)
+
+    max_area = 0
+    for v in areas.values():
+        max_area = max(max_area, v)
+
+    return max_area, middle_size
 
 def main():
     if (len(sys.argv) < 2):
@@ -13,8 +89,9 @@ def main():
     with open(sys.argv[1], 'r') as input:
         data = input.read()
 
-    print('part1', part1(data))
-    print('part2', part2(data))
+    part1, part2 = part12(data)
+    print('part1', part1)
+    print('part2', part2)
 
 if __name__ == '__main__':
     main()
--- day08/main.py
+++ day08/main.py
@@ -1,10 +1,96 @@
 import sys
 
+def sum_metadata(idx, nums):
+    sum = 0
+    if (nums[idx] > 0):
+        for i in range(nums[idx]):
+            s, idx = sum_metadata(idx+2, nums)
+            sum += s
+    else:
+        for i in range(nums[idx+1]):
+            sum += nums[idx+2+i]
+        idx += 2 + nums[idx+1]
+    return sum, idx
+
 def part1(data):
-    lines = data.split('\n')
+    nums = [int(d) for d in data.split(' ')]
+    sum = 0
+    while len(nums) > 0:
+        for i in range(0, len(nums), 2):
+            if nums[i] == 0:
+                for j in range(nums[i+1]):
+                    sum += nums[i+2+j]
+                nums = nums[:i] + nums[i+2+nums[i+1]:]
+                if i > 1:
+                    nums[i-2] -= 1
+                break
+    return sum
+
+def check_part(part):
+    print('check', part, end='')
+    ret = True
+    for i in range(part[0]):
+        ret = ret and (part[i+2] < 1)
+    print(ret)
+    return ret
+
+def sum_part(part):
+    sum = 0
+    start = 2 + part[0]
+    for i in range(start, start + part[1]):
+        if part[i] <= part[1]:
+            sum += part[part[i] + 1]
+    print('sum', sum)
+    return sum
 
 def part2(data):
-    lines = data.split('\n')
+    nums = [int(d) for d in data.split(' ')]
+    sum = 0
+    i = 0
+    # print('before', len(nums), nums[:150])
+    while i < len(nums):
+        if nums[i] == 0:
+            s = 0
+            for j in range(nums[i+1]):
+                s += nums[i+2+j]
+            print('null ->', i, nums[i-2:i+nums[i+1]+nums[i-1]])
+            nums = nums[:i] + [-s] + nums[i+2+nums[i+1]:]
+            print('null <-', i, nums[i-2:i+nums[i+1]+nums[i-1]])
+            if nums[i-2] == 1:
+                i += nums[i-1]
+            i += 1
+        else:
+            i += 2
+
+    i = 0
+    while i < len(nums):
+        if nums[i] == 1 and nums[i+2] < 0:
+            s = sum_part(nums[i:i+3+nums[i+1]])
+            print('one ->', i, nums[i-2:i+nums[i+1]+nums[i-1]])
+            nums = nums[:i] + [s] + nums[i+3+nums[i+1]:]
+            print('one <-', i, nums[i-2:i+nums[i+1]+nums[i-1]])
+            if nums[i-2] == 1:
+                i += nums[i-1]
+            i += 1
+        else:
+            i += 2
+    # print('middle', len(nums), nums[:150])
+    return
+    while len(nums) > 1:
+    # for k in range(3):
+        # print('in', len(nums), nums)
+        for i in range(len(nums)):
+            # print('i', i, nums[i-2:i+1], nums[:150])
+            if nums[i] < 0 and nums[i-2] > 0 and check_part(nums[i-2:i+nums[i-2]]):
+                print('i', i, nums[i-2:i+1], nums[:i+200])
+                s = sum_part(nums[i-2:i+nums[i-2]+nums[i-1]])
+                nums = nums[:i-2] + [s] + nums[i+nums[i-2]+nums[i-1]:]
+                break
+            elif nums[i] < 0 and nums[i-2] > 0:
+                print('i', i, nums[i-2:i+1], nums[:i+200])
+                pass
+
+    return abs(nums[0])
 
 def main():
     if (len(sys.argv) < 2):
