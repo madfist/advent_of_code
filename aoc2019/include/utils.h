e8f82a1559628460022b9235f6d02b63e2cd210a
Adam Koleszar <adam.koleszar@sclable.com>
2020-07-23 08:34:07 +0200

    add deque printer

--- aoc2019/include/utils.h
+++ aoc2019/include/utils.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 
 template<typename T1, typename T2>
 std::ostream& operator<<(std::ostream& os, const std::pair<T1,T2>& s) {
@@ -18,6 +19,17 @@ std::ostream& operator<<(std::ostream& os, const std::vector<char> v) {
 
 template<typename T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T> v) {
+  for (auto i = v.begin(); i != v.end(); ++i) {
+    os << *i;
+    if (i != v.end() - 1) {
+      os << ' ';
+    }
+  }
+  return os;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::deque<T> v) {
   for (auto i = v.begin(); i != v.end(); ++i) {
     os << *i;
     if (i != v.end() - 1) {
