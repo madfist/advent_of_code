b72236923dc2580b48a40d33a54860e483ee6237
Adam Koleszar <madfist@gmail.com>
2021-12-09 16:09:12 +0100

    aoc2021 - Day 5

---
+++ aoc2021/src/dev/madfist/Day05.java
@@ -0,0 +1,140 @@
+package dev.madfist;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Day05 implements Day {
+  private static class Coordinate {
+    int x, y;
+    Coordinate(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Coordinate that = (Coordinate) o;
+      return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+      return "(" + x + "," + y + ")";
+    }
+  }
+
+  private static class Interval {
+    int i, j;
+    Interval(int i, int j) {
+      this.i = Math.min(i, j);
+      this.j = Math.max(i, j);
+    }
+    boolean contains(int n) {
+      return i <= n && n <= j;
+    }
+
+    IntStream range() {
+      return IntStream.range(i, j + 1);
+    }
+  }
+
+  private static class Line {
+    Coordinate a, b;
+    Line(int ax, int ay, int bx, int by) {
+      this.a = new Coordinate(ax, ay);
+      this.b = new Coordinate(bx, by);
+    }
+    boolean isHorizontal() {
+      return a.y == b.y;
+    }
+    boolean isVertical() {
+      return a.x == b.x;
+    }
+    boolean isDiagonal() {
+      return Math.abs(a.x - b.x) == Math.abs(a.y - b.y);
+    }
+    Interval getHorizontalInterval() {
+      return new Interval(a.x, b.x);
+    }
+    Interval getVerticalInterval() {
+      return new Interval(a.y, b.y);
+    }
+    Set<Coordinate> getPoints() {
+      if (isHorizontal()) {
+        return getHorizontalInterval().range().mapToObj(i -> new Coordinate(i, a.y)).collect(Collectors.toSet());
+      } else if (isVertical()) {
+        return getVerticalInterval().range().mapToObj(i -> new Coordinate(a.x, i)).collect(Collectors.toSet());
+      } else if (isDiagonal()) {
+        var x = Math.min(a.x, b.x);
+        var y = a.x < b.x ? a.y : b.y;
+        var yd = (y == a.y ? b.y - a.y : a.y - b.y) / Math.abs(a.y - b.y);
+        return getHorizontalInterval().range().mapToObj(i -> new Coordinate(i, y + yd * (i - x))).collect(Collectors.toSet());
+      }
+      return Set.of();
+    }
+    List<Coordinate> intersect(Line l) {
+      var points = getPoints();
+      points.retainAll(l.getPoints());
+      return points.stream().toList();
+    }
+
+    @Override
+    public String toString() {
+      return "[" + a + "->" + b + "]";
+    }
+  }
+
+  @Override
+  public String solveFirst(List<String> input) {
+    return Long.toString(solve(input, l -> l.isHorizontal() || l.isVertical()));
+  }
+
+  @Override
+  public String solveSecond(List<String> input) {
+    return Long.toString(solve(input, l -> l.isHorizontal() || l.isVertical() || l.isDiagonal()));
+  }
+
+  private Long solve(List<String> input, Predicate<Line> filter) {
+    var pattern = Pattern.compile("(\\d+),(\\d+) -> (\\d+),(\\d+)");
+    var lines = input.stream()
+      .map(str -> {
+        var matcher = pattern.matcher(str);
+        if (matcher.find()) {
+          return new Line(
+            Integer.parseInt(matcher.group(1)),
+            Integer.parseInt(matcher.group(2)),
+            Integer.parseInt(matcher.group(3)),
+            Integer.parseInt(matcher.group(4)));
+        }
+        return new Line(0, 0, 0, 0);
+      })
+      .filter(filter)
+      .toList();
+//    System.out.println(lines);
+    var intersections = new HashMap<Coordinate, Integer>();
+    for (int i = 0; i < lines.size() - 1; ++i) {
+      for (int j = i + 1; j < lines.size(); ++j) {
+        var intersectingCoordinates = lines.get(i).intersect(lines.get(j));
+//        System.out.println("L1: " + lines.get(i) + " L2: " + lines.get(j) + " => " + intersectingCoordinates);
+        if (!intersectingCoordinates.isEmpty()) {
+          intersectingCoordinates.forEach(is -> intersections.merge(is, 1, Integer::sum));
+//          System.out.println("L1: " + lines.get(i) + " L2: " + lines.get(j) + " => " + intersectingCoordinates);
+        }
+      }
+    }
+    return intersections.values().stream().filter(n -> n > 0).count();
+  }
+
+
+}
---
+++ aoc2021/src/dev/madfist/Day05Test.java
@@ -0,0 +1,66 @@
+package dev.madfist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Day05Test {
+
+  @Test
+  void testFirstExample1() {
+    var input = List.of(
+      "0,9 -> 5,9", "8,0 -> 0,8",
+      "9,4 -> 3,4", "2,2 -> 2,1",
+      "7,0 -> 7,4", "6,4 -> 2,0",
+      "0,9 -> 2,9", "3,4 -> 1,4",
+      "0,0 -> 8,8", "5,5 -> 8,2");
+
+    var day5 = new Day05();
+    assertEquals("5", day5.solveFirst(input));
+  }
+
+  @Test
+  void testFirstExample2() {
+    var input = List.of(
+      "0,9 -> 5,9", "8,0 -> 0,8",
+      "9,4 -> 3,4", "2,2 -> 2,1",
+      "7,0 -> 7,4", "6,4 -> 2,0",
+      "0,9 -> 2,9", "3,4 -> 1,4",
+      "0,0 -> 8,8", "5,5 -> 8,2",
+      "4,7 -> 3,7", "4,7 -> 6,7",
+      "2,0 -> 2,1", "9,4 -> 3,4");
+
+    var day5 = new Day05();
+    assertEquals("12", day5.solveFirst(input));
+  }
+
+  @Test
+  void testSecondExample1() {
+    var input = List.of(
+      "0,9 -> 5,9", "8,0 -> 0,8",
+      "9,4 -> 3,4", "2,2 -> 2,1",
+      "7,0 -> 7,4", "6,4 -> 2,0",
+      "0,9 -> 2,9", "3,4 -> 1,4",
+      "0,0 -> 8,8", "5,5 -> 8,2");
+
+    var day5 = new Day05();
+    assertEquals("5", day5.solveSecond(input));
+  }
+
+  @Test
+  void testSecondExample2() {
+    var input = List.of(
+      "0,9 -> 5,9", "8,0 -> 0,8",
+      "9,4 -> 3,4", "2,2 -> 2,1",
+      "7,0 -> 7,4", "6,4 -> 2,0",
+      "0,9 -> 2,9", "3,4 -> 1,4",
+      "0,0 -> 8,8", "5,5 -> 8,2",
+      "4,7 -> 3,7", "4,7 -> 6,7",
+      "2,0 -> 2,1", "9,4 -> 3,4");
+
+    var day5 = new Day05();
+    assertEquals("12", day5.solveSecond(input));
+  }
+}
