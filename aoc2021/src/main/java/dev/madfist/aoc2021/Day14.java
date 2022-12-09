a688b36b6a437e48c0acf1d2ee6512a6077b1508
Adam Koleszar <adam.koleszar@sclable.com>
2022-12-09 15:56:49 +0100

    aoc2021 - Day 14

--- aoc2021/src/main/java/dev/madfist/aoc2021/Day14.java
+++ aoc2021/src/main/java/dev/madfist/aoc2021/Day14.java
@@ -7,66 +7,33 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day14 implements Day {
   @Override
   public String solveFirst(List<String> input) {
-    // var template = input.get(0);
-    // var rules = input.subList(2, input.size()).stream()
-    //   .map(Rule::new)
-    //   .collect(Collectors.toList());
-    // for (int i = 0; i < 10; ++i) {
-    //   var newBuilder = new StringBuilder(template);
-    //   for (int j = 0, k = 1; j < template.length() - 1; ++j) {
-    //     var pair = template.substring(j, j + 2);
-    //     for (var rule: rules) {
-    //       if (rule.pattern.equals(pair)) {
-    //         newBuilder.insert(j + k++, rule.insert);
-    //         break;
-    //       }
-    //     }
-    //   }
-    //   template = newBuilder.toString();
-    // }
-    // var characterCounts = template.chars()
-    //   .mapToObj(c -> (char) c)
-    //   .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-    // var maxCount = characterCounts.values().stream().mapToLong(Long::longValue).max().orElse(-1L);
-    // var minCount = characterCounts.values().stream().mapToLong(Long::longValue).min().orElse(-1L);
-    // return Long.toString(maxCount - minCount);
-    var template = input.get(0);
-    var counter = new CharacterCounter(input.subList(2, input.size()), 10);
-    var characterCounts = counter.count(template);
-    System.out.println(characterCounts);
-    var maxCount = characterCounts.values().stream().mapToLong(Long::longValue).max().orElse(-1L);
-    var minCount = characterCounts.values().stream().mapToLong(Long::longValue).min().orElse(-1L);
-    return Long.toString(maxCount - minCount);
+    return Long.toString(count(input, 10));
   }
 
   @Override
   public String solveSecond(List<String> input) {
+    return Long.toString(count(input, 40));
+  }
+
+  private Long count(List<String> input, int cycles) {
     var template = input.get(0);
-    var counter = new CharacterCounter(input.subList(2, input.size()), 40);
+    var counter = new CharacterCounter(input.subList(2, input.size()), cycles);
     var characterCounts = counter.count(template);
     var maxCount = characterCounts.values().stream().mapToLong(Long::longValue).max().orElse(-1L);
     var minCount = characterCounts.values().stream().mapToLong(Long::longValue).min().orElse(-1L);
-    return Long.toString(maxCount - minCount);
-  }
-
-  private static class Rule {
-    String pattern;
-    char insert;
-
-    public Rule(String line) {
-      pattern = line.substring(0,2);
-      insert = line.charAt(6);
-    }
+    return maxCount - minCount;
   }
 
   private static class CharacterCounter {
-    private Map<Character, Long> counterMap = new ConcurrentHashMap<>();
+    private final Map<Character, Long> counterMap = new ConcurrentHashMap<>();
+    private Map<String, Long> pairOccurrences = new ConcurrentHashMap<>();
     private final int cycles;
-    private Map<String, Character> rules = new HashMap<>();
+    private final Map<String, Character> rules;
 
     public CharacterCounter(List<String> rules, int cycles) {
       this.rules = rules.stream().collect(Collectors.toMap(rule -> rule.substring(0,2), rule -> rule.charAt(6)));
@@ -74,29 +41,26 @@ public class Day14 implements Day {
     }
 
     public Map<Character, Long> count(String text) {
-      var initialList = new ArrayList<String>();
       for (int i = 0; i < text.length() - 1; ++i) {
-        initialList.add(text.substring(i, i + 2));
+        pairOccurrences.merge(text.substring(i, i + 2), 1L, Long::sum);
       }
-      initialList.parallelStream().forEach(pair -> count(pair, 0));
+      for (int i = 0; i < cycles; ++i) {
+        Map<String, Long> pairsToAdd = new HashMap<>();
+        pairOccurrences.forEach((pair, count) -> {
+          var middle = getMiddle(pair);
+          pairsToAdd.merge("" + pair.charAt(0) + middle, count, Long::sum);
+          pairsToAdd.merge("" + middle + pair.charAt(1), count, Long::sum);
+        });
+        pairOccurrences = pairsToAdd;
+      }
+      pairOccurrences.forEach((pair, count) -> {
+        counterMap.merge(pair.charAt(0), count, Long::sum);
+        counterMap.merge(pair.charAt(1), count, Long::sum);
+      });
+      counterMap.replaceAll((ch, count) -> count / 2);
+      counterMap.merge(text.charAt(0), 1L, Long::sum);
       counterMap.merge(text.charAt(text.length() - 1), 1L, Long::sum);
       return counterMap;
-    }
-
-    private void count(String pair, int level) {
-      if (level >= cycles) {
-        return;
-      }
-      //System.out.println(pair + ' ' + level);
-      var middle = getMiddle(pair);
-      if (level == 0) {
-        counterMap.merge(pair.charAt(0), 1L, Long::sum);
-      }
-      if (middle != null) {
-        counterMap.merge(middle, 1L, Long::sum);
-      }
-      count("" + pair.charAt(0) + middle, level + 1);
-      count("" + middle + pair.charAt(1), level + 1);
     }
 
     private Character getMiddle(String pair) {
--- aoc2021/src/test/java/dev/madfist/aoc2021/Day14Test.java
+++ aoc2021/src/test/java/dev/madfist/aoc2021/Day14Test.java
@@ -34,34 +34,9 @@ public class Day14Test {
     assertEquals("1588", day.solveFirst(example));
   }
 
-  @Test
-  public void example3() {
-    var input = Arrays.asList(
-    "NN",
-    "",
-    "CH -> B",
-    "HH -> N",
-    "CB -> H",
-    "NH -> C",
-    "HB -> C",
-    "HC -> B",
-    "HN -> C",
-    "NN -> C",
-    "BH -> H",
-    "NC -> B",
-    "NB -> B",
-    "BN -> B",
-    "BB -> N",
-    "BC -> B",
-    "CC -> N",
-    "CN -> C");
-    var day = new Day14();
-    assertEquals("2188189693529", day.solveSecond(input));
-  }
-
-  // @Test
-  // public void example2() {
-  //   var day = new Day14();
-  //   assertEquals("2188189693529", day.solveSecond(example));
-  // }
+   @Test
+   public void example2() {
+     var day = new Day14();
+     assertEquals("2188189693529", day.solveSecond(example));
+   }
 }
