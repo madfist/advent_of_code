d69df4d7fb5af7c21799330b4f62fbdb3f7f5c03
Adam Koleszar <adam.koleszar@sclable.com>
2022-12-12 16:23:41 +0100

    aoc2022 - Day 11 (WIP)

--- aoc2022/src/main/java/dev/madfist/aoc2022/Day11.java
+++ aoc2022/src/main/java/dev/madfist/aoc2022/Day11.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -11,105 +12,170 @@ import java.util.stream.Collectors;
 public class Day11 implements Day {
   @Override
   public String solveFirst(List<String> input) {
+    return Long.toString(solve(input, 20, true));
+  }
+
+  @Override
+  public String solveSecond(List<String> input) {
+    return Long.toString(solve(input, 10000, false));
+  }
+
+  List<Monkey> parseInput(List<String> input, boolean managedWorryLevel) {
     var numberFunctionPattern = Pattern.compile("old ([+*]) (\\d+)");
     var quadFunctionPattern = Pattern.compile("old ([+*]) old");
     var monkeys = new ArrayList<Monkey>();
     Monkey monkey = null;
-    int divider = 0;
-    int trueMonkey = -1;
     for (var line: input) {
       if (line.startsWith("Monkey")) {
-        monkey = new Monkey();
+        monkey = new Monkey(managedWorryLevel);
       }
-      if (line.startsWith("  Starting") && monkey != null) {
+      if (monkey == null) {
+        continue;
+      }
+      if (line.startsWith("  Starting")) {
         monkey.addItems(Arrays.stream(line.substring(18).split(", "))
           .map(Integer::parseInt).collect(Collectors.toList()));
       }
-      if (line.startsWith("  Operation") && monkey != null) {
+      if (line.startsWith("  Operation")) {
         var opStr = line.substring(19);
-        System.out.println("op " + opStr);
         var opMatcher = numberFunctionPattern.matcher(opStr);
         if (opMatcher.matches()) {
             final var other = Integer.parseInt(opMatcher.group(2));
           if (opMatcher.group(1).equals("+")) {
-            monkey.assignOperation(num -> num + other);
+            monkey.assignOperation((num, div) -> div != 0 ? (num + other) % div : num + other);
           } else {
-            monkey.assignOperation(num -> num * other);
+            monkey.assignOperation((num, div) -> div != 0 ? (num * other) % div : num * other);
           }
         } else {
           var numMatcher = quadFunctionPattern.matcher(opStr);
           if (numMatcher.matches()) {
-            monkey.assignOperation(opMatcher.group(1).equals("+")
-              ? num -> num + num
-              : num -> num * num);
+            monkey.assignOperation(numMatcher.group(1).equals("+")
+              ? (num, div) -> div != 0 ? (num + num) % div : num + num
+              : (num, div) -> div != 0 ? (num * num) % div : num * num);
           }
         }
       }
       if (line.startsWith("  Test")) {
-        divider = Integer.parseInt(line.substring(21));
+        monkey.setDivider(Integer.parseInt(line.substring(21)));
       }
       if (line.startsWith("    If true")) {
-        trueMonkey = Integer.parseInt(line.substring(29));
+        monkey.setNextMonkeyIndex1(Integer.parseInt(line.substring(29)));
       }
-      if (line.startsWith("    If false") && monkey != null) {
-        final int d = divider;
-        final int t = trueMonkey;
-        final int f = Integer.parseInt(line.substring(30));
-        monkey.assignTest(item -> item % d == 0 ? t : f);
+      if (line.startsWith("    If false")) {
+        monkey.setNextMonkeyIndex2(Integer.parseInt(line.substring(30)));
       }
       if (line.isBlank()) {
         monkeys.add(monkey);
       }
     }
-    for (int i = 0; i < 20; ++i) {
+    if (monkeys.isEmpty() || !monkey.equals(monkeys.get(monkeys.size() - 1))) {
+      monkeys.add(monkey);
+    }
+    if (!managedWorryLevel) {
+      monkeys.forEach(Monkey::modItems);
+    }
+    return monkeys;
+  }
+
+  long solve(List<String> input, int cycles, boolean managedWorryLevel) {
+    var monkeys = parseInput(input, managedWorryLevel);
+    for (int i = 0; i < cycles; ++i) {
+      System.out.println(i + ". " + monkeys);
       for (var m : monkeys) {
         var nextMonkeys = m.turn();
-        for (int n = 0; n < nextMonkeys.size(); ++n) {
-          m.throwItem(n, monkeys.get(nextMonkeys.get(n)));
+        System.out.println(nextMonkeys);
+        for (var nextMonkey : nextMonkeys) {
+          m.throwFirstItem(monkeys.get(nextMonkey));
         }
       }
     }
-    monkeys.sort(Comparator.comparing(m -> m.inspections));
-    var result = monkeys.get(0).inspections * monkeys.get(1).inspections;
-    return Integer.toString(result);
+    monkeys.sort(Comparator.comparing(Monkey::getInspections).reversed());
+    System.out.println(monkeys.stream().map(Monkey::getInspections).collect(Collectors.toList()));
+    return monkeys.get(0).inspections * monkeys.get(1).inspections;
   }
 
-  @Override
-  public String solveSecond(List<String> input) {
-    return null;
-  }
-
-  private static class Monkey {
+  static class Monkey {
+    private final boolean managedWorryLevel;
     List<Integer> items;
-    Function<Integer, Integer> operation;
-    Function<Integer, Integer> test;
-    int inspections = 0;
+    BiFunction<Integer, Integer, Integer> operation;
+    long inspections = 0;
+    int divider = 0;
+    int nextMonkeyIndex1;
+    int nextMonkeyIndex2;
+
+    public Monkey(boolean managedWorryLevel) {
+      this.managedWorryLevel = managedWorryLevel;
+    }
 
     public void addItems(List<Integer> items) {
       this.items = items;
     }
 
-    public void assignOperation(Function<Integer, Integer> operation) {
-      this.operation = operation;
+    public void modItems() {
+      this.items = this.items.stream().map(item -> item % divider).collect(Collectors.toList());
     }
 
-    public void assignTest(Function<Integer, Integer> test) {
-      this.test = test;
+    public void assignOperation(BiFunction<Integer, Integer, Integer> operation) {
+      this.operation = operation;
     }
 
     public List<Integer> turn() {
       var itemThrows = new ArrayList<Integer>(items.size());
-      for (var item: items) {
-        var newValue = Math.floorDiv(operation.apply(item), 3);
-        itemThrows.add(test.apply(newValue));
+      for (int i = 0; i < items.size(); ++i) {
+        items.set(i, getOperation().apply(items.get(i)));
+        itemThrows.add(items.get(0) % divider == 0 ? nextMonkeyIndex1 : nextMonkeyIndex2);
         ++inspections;
       }
       return itemThrows;
     }
 
-    public void throwItem(int index, Monkey other) {
-      other.items.add(items.get(index));
-      items.remove(index);
+    public void throwFirstItem(Monkey other) {
+      other.items.add(items.get(0));
+      items.remove(0);
+    }
+
+    public List<Integer> getItems() {
+      return items;
+    }
+
+    public Function<Integer, Integer> getOperation() {
+      if (managedWorryLevel) {
+        return num -> Math.floorDiv(operation.apply(num, 0), 3);
+      }
+      return num -> operation.apply(num, divider);
+    }
+
+    public long getInspections() {
+      return inspections;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("%s(%d)", items, inspections);
+    }
+
+    public int getDivider() {
+      return divider;
+    }
+
+    public void setDivider(int divider) {
+      this.divider = divider;
+    }
+
+    public int getNextMonkeyIndex1() {
+      return nextMonkeyIndex1;
+    }
+
+    public void setNextMonkeyIndex1(int nextMonkeyIndex1) {
+      this.nextMonkeyIndex1 = nextMonkeyIndex1;
+    }
+
+    public int getNextMonkeyIndex2() {
+      return nextMonkeyIndex2;
+    }
+
+    public void setNextMonkeyIndex2(int nextMonkeyIndex2) {
+      this.nextMonkeyIndex2 = nextMonkeyIndex2;
     }
   }
 }
--- aoc2022/src/test/java/dev/madfist/aoc2022/Day11Test.java
+++ aoc2022/src/test/java/dev/madfist/aoc2022/Day11Test.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Day11Test {
   List<String> example = Arrays.asList(
@@ -38,6 +38,40 @@ public class Day11Test {
     "    If false: throw to monkey 1");
 
   @Test
+  public void inputTest() {
+    var singleExample = List.of(
+      "Monkey 0:",
+      "  Starting items: 79, 98",
+      "  Operation: new = old * 19",
+      "  Test: divisible by 23",
+      "    If true: throw to monkey 2",
+      "    If false: throw to monkey 3");
+    var day = new Day11();
+    var monkeysManaged = day.parseInput(singleExample, true);
+    assertEquals(1, monkeysManaged.size());
+    assertIterableEquals(List.of(79, 98), monkeysManaged.get(0).getItems());
+    assertEquals(63, monkeysManaged.get(0).getOperation().apply(10));
+    assertEquals(23, monkeysManaged.get(0).getDivider());
+    assertEquals(2, monkeysManaged.get(0).getNextMonkeyIndex1());
+    assertEquals(3, monkeysManaged.get(0).getNextMonkeyIndex2());
+    assertIterableEquals(List.of(3, 3), monkeysManaged.get(0).turn());
+    assertIterableEquals(List.of(500, 620), monkeysManaged.get(0).getItems());
+    assertEquals(2, monkeysManaged.get(0).getInspections());
+
+    var monkeysUnmanaged = day.parseInput(singleExample, false);
+    assertEquals(1, monkeysManaged.size());
+    assertEquals(23, monkeysManaged.get(0).getDivider());
+    assertIterableEquals(List.of(10, 6), monkeysUnmanaged.get(0).getItems());
+    assertEquals(6, monkeysUnmanaged.get(0).getOperation().apply(10));
+    assertEquals(23, monkeysUnmanaged.get(0).getDivider());
+    assertEquals(2, monkeysUnmanaged.get(0).getNextMonkeyIndex1());
+    assertEquals(3, monkeysUnmanaged.get(0).getNextMonkeyIndex2());
+    assertIterableEquals(List.of(3, 3), monkeysUnmanaged.get(0).turn());
+    assertIterableEquals(List.of(6, 22), monkeysUnmanaged.get(0).getItems());
+    assertEquals(2, monkeysUnmanaged.get(0).getInspections());
+  }
+
+  @Test
   public void example1() {
     var day = new Day11();
     assertEquals("10605", day.solveFirst(example));
@@ -46,6 +80,8 @@ public class Day11Test {
   @Test
   public void example2() {
     var day = new Day11();
-    assertEquals(null, day.solveSecond(example));
+    assertEquals(24L, day.solve(example, 1, false));
+    assertEquals(10197L, day.solve(example, 20, false));
+    assertEquals("2713310158", day.solveSecond(example));
   }
 }
